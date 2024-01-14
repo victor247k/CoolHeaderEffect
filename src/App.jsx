d07510eb44968538599bee898b51b56d013ee1e2
victor247k <topgspineivictor@gmail.com>
2024-01-14 21:28:57 +0200

    just the base and just the initial animation still have a lot to add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <header className="cool-header">
+      <h1 className="original-text">A Quick And Easy Guide To Tmux</h1>
+      <p className="description-text">Published on Aug 16, 2015</p>
+      <p className="description-text">Last updated on Nov 22, 2022</p>
+      <p className="description-text tooltip">About 12 minutes reading time 🍫</p>
+      <div className="color-red"></div>
+      <div className="color-yellow"></div>
+      <div className="color-green"></div>
+    </header>
+  )
+}
+
+export default App
